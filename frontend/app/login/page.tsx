ece175694eb64799d3c22939dbf1a90e217273b5
JUHYE0925 <rhkdlf48301@gmail.com>
2026-01-15 21:17:18 +0900

    change backend url

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -6,8 +6,9 @@ import { SiKakaotalk, SiNaver } from "react-icons/si";
 import { useAuth } from '../context/AuthContext'; // 1. useAuth import
 import { User } from '../types'; // User 타입 import
 
-const BACKEND_URL = "http://localhost:8080";
-const BASE_AUTH_URL : string = `${BACKEND_URL}/api/v1/auth`;
+// const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://api.ourhour.cloud";
+const BASE_AUTH_URL : string = `${BACKEND_URL}/api/v1`;
 const getButtonStyle = (base: string, bgColor: string, hoverColor: string) => 
     `${base} ${bgColor} ${hoverColor} w-full flex items-center justify-center gap-3 text-lg py-2 px-4 rounded-lg font-semibold transition duration-150 ease-in-out`;
 
